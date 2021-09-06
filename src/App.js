73f0fa8b7fa8af730909a22a1bcd8bcd04ce65e3
Shahbaazkyz <Shahbaazkyz@gmail.com>
2021-09-06 21:00:35 +0500

    V1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [cityName, setCity] = useState();
+  const [weather, setWeather] = useState();
+
+  const search = (e) => {
+    e.preventDefault();
+    // Make a request for a user with a given ID
+    axios
+      .get(
+        `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=a1a36875d10fc7c4646740447a2926d0`
+      )
+      .then(function (response) {
+        // handle success
+        console.log(response);
+        console.log(response.data);
+        const data = [response.data.main];
+        setWeather(response);
+        console.log(weather);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
+  };
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Weather App</h1>
+        <form onSubmit={search}>
+          <input
+            type="text"
+            value={cityName}
+            onChange={(e) => setCity(e.target.value)}
+          />
+          <button type="submit">Search</button>
+        </form>
+        <div className="report">
+          {weather ? (
+            <>
+              <h3>City : {weather.data.name}</h3>
+              <h3>
+                {" "}
+                Temperature : {(weather.data.main.temp - 273.15).toFixed(
+                  2
+                )} °C{" "}
+              </h3>
+              <h3>
+                {" "}
+                Feels like :{" "}
+                {(weather.data.main.feels_like - 273.15).toFixed(2)} °C{" "}
+              </h3>
+              <p>{(weather.data.weather[0].description).toUpperCase()}</p>
+            </>
+          ) : (
+            <h3> </h3>
+          )}
+        </div>
       </header>
     </div>
   );
