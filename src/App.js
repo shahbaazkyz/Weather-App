cd1786ed082e1f2423254405b9fd5e31c429c252
Shahbaazkyz <Shahbaazkyz@gmail.com>
2021-09-07 19:28:08 +0500

    V2

--- src/App.js
+++ src/App.js
@@ -28,42 +28,71 @@ function App() {
       .then(function () {
         // always executed
       });
+    
+    setCity("")
   };
+
+  let date = new Date();
+  let currDate = date.getDate();
+  let currMonth = date.toLocaleString("en-us" , {month : "long"});
+  let year = date.getFullYear();
 
 
   return (
     <div className="App">
       <header className="App-header">
+        {weather ? (
+          <div className="report">
+            <div className="left">
+              <h1 className="city">{weather.data.name}</h1>
+              <p>
+                {currMonth + " " + currDate}, {year}
+              </p>
+              <img
+                src={`http://openweathermap.org/img/wn/${weather.data.weather[0].icon}@2x.png`}
+                alt=""
+              />
+           <b> <h5>{weather.data.weather[0].description.toUpperCase()}</h5></b>
+            </div>
+            <div className="right">
+              <h1 className="tempFont">
+                {(weather.data.main.temp - 273.15).toFixed()}°
+              </h1>
+              <h3>
+                {(weather.data.main.temp_max - 273.15).toFixed()}
+                °/
+                <span className="minTemp">
+                  {(weather.data.main.temp_min - 273.15).toFixed()}°
+                </span>
+              </h3>
+              <h3>
+                Feels like : {(weather.data.main.feels_like - 273.15).toFixed()}
+                °
+              </h3>
+              <h3>Humidity : {weather.data.main.humidity}</h3>
+            </div>
+          </div>
+        ) : (
+          <h3> </h3>
+        )}
         <h1>Weather App</h1>
+
         <form onSubmit={search}>
-          <input
-            type="text"
-            value={cityName}
-            onChange={(e) => setCity(e.target.value)}
-          />
-          <button type="submit">Search</button>
+          <div class="input-group mb-3">
+            <input
+              type="text"
+              className="form-control border  rounded-top  "
+              value={cityName}
+              placeholder="Enter City Name"
+              aria-label="Entery City Name"
+              aria-describedby="button-addon2"
+              onChange={(e) => setCity(e.target.value)}
+            />
+            <button type="submit" className="btn btn-light" id="button-addon2">
+              Search
+            </button>
+          </div>
         </form>
-        <div className="report">
-          {weather ? (
-            <>
-              <h3>City : {weather.data.name}</h3>
-              <h3>
-                {" "}
-                Temperature : {(weather.data.main.temp - 273.15).toFixed(
-                  2
-                )} °C{" "}
-              </h3>
-              <h3>
-                {" "}
-                Feels like :{" "}
-                {(weather.data.main.feels_like - 273.15).toFixed(2)} °C{" "}
-              </h3>
-              <p>{(weather.data.weather[0].description).toUpperCase()}</p>
-            </>
-          ) : (
-            <h3> </h3>
-          )}
-        </div>
       </header>
     </div>
   );
